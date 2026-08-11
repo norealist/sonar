801cdb2a85ba657586e00a5a63267e8edd60d6a8
norealist <ivanlein777@gmail.com>
2026-08-11 17:26:10 +0300

    Move Android runtime assets into app module

--- sonar-app/build.gradle.kts
+++ sonar-app/build.gradle.kts
@@ -1,23 +1,6 @@
-import org.gradle.api.tasks.Sync
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
-}
-
-val syncUiAssets = tasks.register<Sync>("syncUiAssets") {
-    val source = rootProject.layout.projectDirectory.dir("ui")
-    from(source) {
-        include("logo.png", "logo2.png", "deezer-logo.png")
-        include("fonts/Disco.ttf")
-    }
-    into(layout.buildDirectory.dir("generated/uiAssets"))
-}
-
-val syncUiResources = tasks.register<Sync>("syncUiResources") {
-    from(rootProject.layout.projectDirectory.file("ui/fonts/Disco.ttf"))
-    into(layout.buildDirectory.dir("generated/uiRes/font"))
-    rename { "disco.ttf" }
 }
 
 android {
@@ -49,9 +32,6 @@ android {
         buildConfig = true
     }
 
-    sourceSets["main"].assets.srcDir(layout.buildDirectory.dir("generated/uiAssets").get().asFile)
-    sourceSets["main"].res.srcDir(layout.buildDirectory.dir("generated/uiRes").get().asFile)
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -62,11 +42,6 @@ android {
             jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
         }
     }
-}
-
-tasks.named("preBuild") {
-    dependsOn(syncUiAssets)
-    dependsOn(syncUiResources)
 }
 
 dependencies {
