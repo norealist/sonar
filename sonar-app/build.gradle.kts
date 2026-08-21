7de97b168629c31898598519a28b5e92f47e5d2a
norealist <ivanlein777@gmail.com>
2026-08-21 15:19:49 +0300

    chore(release): bump version to pre 1.2 (versionCode 4)

--- sonar-app/build.gradle.kts
+++ sonar-app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sonar.app"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "pre 1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
